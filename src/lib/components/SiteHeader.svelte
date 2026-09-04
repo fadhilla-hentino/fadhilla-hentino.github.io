6ce4ec6361a0a7497b7e988b99ecd5933642f302
fadhilla <fadhilla@accelbyte.net>
2026-09-04 22:01:50 +0700

    fix site header

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { navigationItems, profile } from '$lib/content/profile';
+	import { navigationItems } from '$lib/content/profile';
 
 	let menuOpen = false;
 	let activeSection = 'top';
